400fde79851e1c49f531be6dd0cff8c905ec68e2
tmdgusya <tmdgusya@naver.com>
2022-10-03 01:14:00 +0900

    write hibernate issue code

--- src/main/kotlin/com/roach/kotlnjsr303/TestController.kt
+++ src/main/kotlin/com/roach/kotlnjsr303/TestController.kt
@@ -1,12 +1,13 @@
 package com.roach.kotlnjsr303
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.ConstraintViolationException
 import javax.validation.constraints.Min
 
 @RestController
@@ -19,5 +20,10 @@ class TestController {
     @GetMapping("/test")
     suspend fun test(@RequestParam @Min(0) no: Int) = withContext(Dispatchers.Default) {
         return@withContext "Success $no"
+    }
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun exceptionHandler(e: ConstraintViolationException): String? {
+        return e.message
     }
 }
