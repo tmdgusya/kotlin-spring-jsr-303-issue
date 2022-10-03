1192ad513e9402e8276f6cb305d4e038142dc415
tmdgusya <tmdgusya@naver.com>
2022-10-03 16:46:31 +0900

    Tip 추가

--- src/main/kotlin/com/roach/kotlnjsr303/TestController.kt
+++ src/main/kotlin/com/roach/kotlnjsr303/TestController.kt
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
@@ -15,9 +14,6 @@ import org.springframework.web.bind.annotation.RestController
 import javax.validation.ConstraintViolationException
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
-import kotlin.jvm.internal.Reflection
-import kotlin.reflect.full.declaredMembers
-import kotlin.reflect.full.findAnnotation
 
 @RestController
 @Validated
